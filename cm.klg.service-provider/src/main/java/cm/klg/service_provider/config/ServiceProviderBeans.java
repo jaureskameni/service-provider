678a10039062213a2ccd0e3ae4c98f3b82fbb5ad
JAURES KAMENI NGUEMATCHA <kameni.nguematcha@lao-sarl.cm>
2026-05-08 10:15:51 +0100

    refactor(SP-4): add bean of jwtGrantedAuthoritiesConverter class

--- cm.klg.service-provider/src/main/java/cm/klg/service_provider/config/ServiceProviderBeans.java
+++ cm.klg.service-provider/src/main/java/cm/klg/service_provider/config/ServiceProviderBeans.java
@@ -1,6 +1,14 @@
 package cm.klg.service_provider.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration
-public class ServiceProviderBeans {}
+public class ServiceProviderBeans {
+
+  @Bean
+  public JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter() {
+    return new JwtGrantedAuthoritiesConverter();
+  }
+}
